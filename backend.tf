84c8d5b1e5597168397982af39ef942480cd1d1c
Mor fall <38078533+fallmor@users.noreply.github.com>
2021-02-24 20:59:17 +0100

    test

--- backend.tf
+++ backend.tf
@@ -54,3 +54,26 @@ resource "aws_s3_bucket_policy" "terraform_state" {
 }
 EOF
 }
+
+esource "aws_s3_bucket" "wordpress" {
+  bucket_prefix = "morfall-"
+  force_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags =  "Project" = "eks"
+}
+
+## S3 Bucket Public Access Block
+resource "aws_s3_bucket_public_access_block" "vault_data" {
+  bucket = aws_s3_bucket.wordpress.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = true
+  restrict_public_buckets = false
+}
